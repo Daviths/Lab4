f166fb6b663a0804680c6473219b83ad25c5fd5e
Daviths <davidelga47@hotmail.com>
2014-10-01 22:56:32 -0300

    Production db test

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,14 @@
 # encoding = utf-8
 class TweetsController < ApplicationController
-  
+
+	def create
+		@tweet = Tweet.new		
+		@tweet.estado = params[:tweet][:estado]
+		@tweet.monstruo_id = params[:tweet][:monstruo_id]
+		@tweet.save
+		redirect_to monstruos_path
+	end  
+
   def new
   	@tweet = Tweet.new
   end
@@ -9,8 +17,13 @@ class TweetsController < ApplicationController
   end
   
   def index
+	@monstruo = Monstruo.find(params[:id])	
+	if @monstruo.tweets.count > 2
+		@monstruo.critter_star = true
+	end
   end
 
   def show
+	
   end
 end
